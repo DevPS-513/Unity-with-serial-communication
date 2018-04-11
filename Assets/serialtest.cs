ba8e2798643ecd5a3e7efd2e6279ff9d2d100bbd
onion117 <max.bethune.waddell@gmail.com>
2018-04-11 09:46:53 -0700

    first commit

---
+++ Assets/serialtest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class serialtest : MonoBehaviour {
+
+
+    private float AmountToMove;
+
+
+    // print(SerialPort.GetPortNames());
+    // Use this for initialization
+    void Start () {
+
+        SerialPort serial = new SerialPort("\\.\COM14", 9600);
+
+       // SerialPort serial = new SerialPort("COM14", 9600);
+
+
+        serial.Open();
+
+        serial.WriteLine("connect_to_unity 1");
+        StartCoroutine("DoSomething"); // Pause
+
+        print(serial.ReadLine());
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Always check if it is closed?
+        //  if (!serial.IsOpen)
+        //   serial.Open(); 
+
+        //  int number = int.Parse (serial.ReadLine());
+
+        //Debug.Log("Text");
+
+    }
+
+
+
+    IEnumerator DoSomething()
+    {
+        yield return new WaitForSeconds(5f);
+        print("Ok");
+    }
+}
+
+
+//   print("serial ports: \n");
+//  string[] ports = SerialPort.GetPortNames();
+
+//      foreach (string port in ports)
+//      {
+//  print(port);
+//      }
